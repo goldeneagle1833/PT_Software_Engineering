3670216569629deb3f7ac8d5b5f38a8fe8c26f3c
goldeneagle1833 <55966342+goldeneagle1833@users.noreply.github.com>
2020-06-14 17:05:44 -0400

    crazyObject
    complete

--- Mod1/week-3/w3d3/homework/answers.js
+++ Mod1/week-3/w3d3/homework/answers.js
@@ -26,3 +26,46 @@ for(key in bruce){
         console.log("hobby is running")
     }
 }
+
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
+
+
+
+console.log(crazyObject.taco[1].salsa[5])
+console.log(crazyObject.larry.quotes[0])
+console.log(crazyObject.larry.characters[2].favourtieHobby)
+console.log(crazyObject.larry.nicknames[1])
+ 
+for (key in crazyObject.larry.characters[1]){
+    if(key === "name") {
+        console.log(crazyObject.larry.characters[1])
+    }
+  }
+
+crazyObject.larry.quotes.push("new quote")
+
+console.log(crazyObject.larry.quotes)
