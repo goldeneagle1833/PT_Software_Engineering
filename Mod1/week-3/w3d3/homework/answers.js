c7d4a5131bec90e8eaaffd3eef890355d775b08f
goldeneagle1833 <55966342+goldeneagle1833@users.noreply.github.com>
2020-06-14 16:00:00 -0400

    take it easy
    took it easy

--- Mod1/week-3/w3d3/homework/answers.js
+++ Mod1/week-3/w3d3/homework/answers.js
@@ -6,3 +6,23 @@
 5) array of objects
 6) objects with arrays
 */
+
+const rainbowColors = ["red", "blue", "violet", "green"]
+
+for (let i = 0; i<rainbowColors.length; i++){
+    if (rainbowColors[i] === "blue"){
+        console.log("blue")
+    }
+}
+
+const bruce = {
+    favoriteFood: "steak",
+    hobby: "running",
+    town: "baltiomore",
+    favoriteData: true
+}
+for(key in bruce){
+    if(bruce[key] === "running"){
+        console.log("hobby is running")
+    }
+}
